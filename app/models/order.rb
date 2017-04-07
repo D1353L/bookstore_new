20b078f7d0ff16393452da2a9f1dedba81de2c02
D1353L <meghanik400@gmail.com>
2017-04-07 12:21:00 +0300

    Addresses and coupon relations changed to optional

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   has_many :order_items
   belongs_to :order_status
   belongs_to :user
-  belongs_to :coupon
-  belongs_to :billing_address, :class_name => 'Address'
-  belongs_to :shipping_address, :class_name => 'Address'
+  belongs_to :coupon, optional: true
+  belongs_to :billing_address, :class_name => 'Address', optional: true
+  belongs_to :shipping_address, :class_name => 'Address', optional: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
   has_many :credit_cards
   belongs_to :user
-  belongs_to :billing_address, :class_name => 'Address'
-  belongs_to :shipping_address, :class_name => 'Address'
+  belongs_to :billing_address, :class_name => 'Address', optional: true
+  belongs_to :shipping_address, :class_name => 'Address', optional: true
 end
